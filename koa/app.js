94b635dcb16e1b717a0f593ce9b2b774c1b68c4d
邵佳迪 <1923943838qq.com>
2021-01-26 19:10:28 +0800

    koa添加静态资源中间件

--- koa/app.js
+++ koa/app.js
@@ -6,6 +6,7 @@ var Koa = require('koa'),
     router = require('koa-router')(), /*引入是实例化路由*/
     common = require('./module/common.js'),
     bodyParser = require('koa-bodyparser');
+    static = require('koa-static');
 
 /* var Router = require('koa-router');
 var router = new Router(); */
@@ -21,6 +22,12 @@ var app = new Koa();
 app.use(view('view',{
   extension: 'ejs'      /*应用ejs模板引擎*/
 }))
+
+//http://localhost:3000/css/basic.css 首先去static目录找，如果能找到返回对应文件，找不到next()
+//配置静态web服务中间件
+app.use(static('static'));     //静态web服务中间件可以配置多个
+// app.use(static(__dirname+'/static'));  //同上
+
 
 
 //配置post bodyparser的中间件
